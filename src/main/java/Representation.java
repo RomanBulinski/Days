2e11b4396bf0f3344f94eb795816424d15a69100
roman <aboandrus@gmail.com>
2019-10-14 21:58:40 +0200

    Implemented code

--- src/main/java/Representation.java
+++ src/main/java/Representation.java
@@ -1,7 +1,14 @@
 public class Representation {
 
-    public static int daysRepresented (int [][] trips){
-        /* your code here */
+    public static int daysRepresented(int[][] trips) {
 
+        int res = 0;
+        for (int i = 0; i < trips.length; i++) {
+            res = res + trips[i][1] - trips[i][0];
+        }
+
+        return  res;
     }
+
+}
 }
