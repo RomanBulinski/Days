da4c07a28979b566e67a953ffec75e7b8cb3212a
roman <aboandrus@gmail.com>
2019-10-14 22:03:06 +0200

    Refactor code

--- src/main/java/Representation.java
+++ src/main/java/Representation.java
@@ -1,14 +1,12 @@
 public class Representation {
 
     public static int daysRepresented(int[][] trips) {
-
         int res = 0;
         for (int i = 0; i < trips.length; i++) {
-            res = res + trips[i][1] - trips[i][0];
+            res = res + trips[i][1] - trips[i][0] + 1;
         }
-
         return  res;
     }
 
 }
-}
+
